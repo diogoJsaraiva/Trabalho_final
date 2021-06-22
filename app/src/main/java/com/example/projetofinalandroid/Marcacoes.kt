7cce250ae602ce25a9d0b75b97e4d4bbda75b98e
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-06-22 21:59:58 +0100

    Atualizar objeto Marcacoes

--- app/src/main/java/com/example/projetofinalandroid/Marcacoes.kt
+++ app/src/main/java/com/example/projetofinalandroid/Marcacoes.kt
@@ -4,13 +4,11 @@ import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
 
-class Marcacoes(var id: Long = -1, var dose1: Int, var checkdose1: String, var dose2: Int, var checkdose2: String, var idVacina: Long, var idPessoa : Long) {
+class Marcacoes(var id: Long = -1, var datadose: Int, var numero_dose: String,var idVacina: Long, var idPessoa : Long) {
     fun toContentValues(): ContentValues {
         val valores = ContentValues()
-        valores.put(TabelaMarcacoes.CAMPO_DATA_DOSE_1, dose1)
-        valores.put(TabelaMarcacoes.CAMPO_CHECK_DOSE_1, checkdose1)
-        valores.put(TabelaMarcacoes.CAMPO_DATA_DOSE_2, dose2)
-        valores.put(TabelaMarcacoes.CAMPO_CHECK_DOSE_2, checkdose2)
+        valores.put(TabelaMarcacoes.CAMPO_DATA_DOSE, datadose)
+        valores.put(TabelaMarcacoes.CAMPO_NUMERO_DOSE, numero_dose)
         valores.put(TabelaMarcacoes.CAMPO_ID_VACINA, idVacina)
         valores.put(TabelaMarcacoes.CAMPO_ID_VACINA, idPessoa)
 
@@ -21,23 +19,19 @@ class Marcacoes(var id: Long = -1, var dose1: Int, var checkdose1: String, var d
         fun fromCursor(cursor: Cursor): Marcacoes {
             val colId = cursor.getColumnIndex(BaseColumns._ID)
 
-            val coldose1 = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_DATA_DOSE_1)
-            val colCheckdose1 = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_CHECK_DOSE_1)
-            val coldose2 = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_DATA_DOSE_2)
-            val colcheckdose2 = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_CHECK_DOSE_2)
+            val coldatadose = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_DATA_DOSE)
+            val colnumerodose = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_NUMERO_DOSE)
             val colvacina = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_ID_VACINA)
             val colpessoa = cursor.getColumnIndex(TabelaMarcacoes.CAMPO_ID_PESSOA)
 
 
             val id = cursor.getLong(colId)
-            val dose1 = cursor.getInt(coldose1)
-            val check1 = cursor.getString(colCheckdose1)
-            val dose2 = cursor.getInt(coldose2)
-            val check2 = cursor.getString(colcheckdose2)
+            val datadose = cursor.getInt(coldatadose)
+            val numerodose = cursor.getString(colnumerodose)
             val vacina = cursor.getLong(colvacina)
             val pessoa = cursor.getLong(colpessoa)
 
-            return Marcacoes(id, dose1,check1,dose2,check2,vacina,pessoa)
+            return Marcacoes(id, datadose,numerodose,vacina,pessoa)
         }
     }
 }
